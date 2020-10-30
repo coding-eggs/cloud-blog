c7e05e842e4bb7898b497f748b9bcfb2f903bc29
coding-eggs <2665151959@qq.com>
2020-10-31 03:09:14 +0800

    整理OAuth2 完善架构

--- blog-gateway/src/main/java/com/cloud/blog/gateway/config/oauth/JwtRestTemplateConfig.java
+++ blog-gateway/src/main/java/com/cloud/blog/gateway/config/oauth/JwtRestTemplateConfig.java
@@ -13,7 +13,7 @@ import java.util.Collections;
 
 
 /**
-* 在部署得时候 获取公钥得地址考虑到高可用，所以得采用负载均衡，利用nacos。通过看源码知道，请求公钥是restTemplate来完成得，这个template没有
+* 在部署得时候 获取公钥得地址考虑到高可用，所以得采用负载均衡，利用 nacos。通过看源码知道，请求公钥是restTemplate来完成得，这个template没有
  * 加LoadBalanced注解，所以没有负载均衡得功能，阅读源码知道，初始化时会扫描 {@link JwtAccessTokenConverterRestTemplateCustomizer}这个类
  * 然后指定对restTemplate得配置，默认时没有得，这里我们实现一个这个类，从源码中我们得知LoadBalanced实现负载其实就是一个拦截器，这里我们生创建一个拦截器
  * 放进这个restTemplate。 其实还有第二种方案就是重写jwtTokenStore，但是比较麻烦。
